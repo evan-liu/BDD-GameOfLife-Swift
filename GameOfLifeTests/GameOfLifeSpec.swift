66a575410dc7a95ff4283ccde711fb7111e5ac74
Evan Liu <evancoding@gmail.com>
2015-05-21 10:35:37 +1200

    Rename GameOfLifeTests.swift to GameOfLifeSpec.swift

---
+++ GameOfLifeTests/GameOfLifeSpec.swift
@@ -0,0 +1,16 @@
+//
+//  GameOfLifeTests.swift
+//  GameOfLifeTests
+//
+//  Created by Evan on 21/05/15.
+//  Copyright (c) 2015 evancoding. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class GameOfLifeSpec: QuickSpec {
+    override func spec() {
+        
+    }
+}
