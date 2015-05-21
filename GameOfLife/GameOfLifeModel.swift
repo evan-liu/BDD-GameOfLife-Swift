6c4652b6fc2980592eba6d9385f0379d2d6ed921
Evan Liu <evancoding@gmail.com>
2015-05-22 09:58:27 +1200

    Rule #1

---
+++ GameOfLife/GameOfLifeModel.swift
@@ -0,0 +1,52 @@
+//
+//  GameOfLifeModel.swift
+//  GameOfLife
+//
+//  Created by Evan on 21/05/15.
+//  Copyright (c) 2015 evancoding. All rights reserved.
+//
+
+import Foundation
+
+public class GameOfLifeModel {
+    public var rows: Int
+    public var columns: Int
+    
+    private var aliveMap = [String:Bool]()
+    
+    public init(rows:Int, columns:Int) {
+        self.rows = rows
+        self.columns = columns;
+    }
+    
+    public func isCellAliveAtRow(row: Int, column: Int) -> Bool {
+        return aliveMap[keyOfCellAtRow(row, column: column)] == true
+    }
+    
+    public func makeCellBeAliveAtRow(row: Int, column: Int) {
+        aliveMap[keyOfCellAtRow(row, column: column)] = true
+    }
+    
+    public func willCellBeAliveAtRow(row: Int, column: Int) -> Bool {
+        var aliveNeighbours = 0
+        for i in row - 1...row + 1 {
+            for j in column - 1...column + 1 {
+                if !(i == row && j == column) && isCellAliveAtRow(i, column: j) {
+                    aliveNeighbours++
+                }
+            }
+        }
+        
+        if isCellAliveAtRow(row, column: column) {
+            if aliveNeighbours < 2 {
+                return false
+            }
+        }
+        
+        return false
+    }
+    
+    private func keyOfCellAtRow(row: Int, column: Int) -> String {
+        return "\(row)_\(column)";
+    }
+}
--- GameOfLifeTests/GameOfLifeSpec.swift
+++ GameOfLifeTests/GameOfLifeSpec.swift
@@ -8,9 +8,28 @@
 
 import Quick
 import Nimble
+import GameOfLife
 
 class GameOfLifeSpec: QuickSpec {
     override func spec() {
+        var model:GameOfLifeModel!
+        beforeEach {
+            model = GameOfLifeModel(rows: 3, columns: 3)
+        }
         
+        describe("Rule #1") {
+            context("A live cell with fewer than 2 live neighbours") {
+                it("will die") {
+                    model.makeCellBeAliveAtRow(1, column: 1)
+                    
+                    // 0 live neighbours
+                    expect(model.willCellBeAliveAtRow(1, column: 1)).to(beFalse())
+                    
+                    // 1 live neighbour
+                    model.makeCellBeAliveAtRow(0, column: 0)
+                    expect(model.willCellBeAliveAtRow(1, column: 1)).to(beFalse())
+                }
+            }
+        }
     }
 }
